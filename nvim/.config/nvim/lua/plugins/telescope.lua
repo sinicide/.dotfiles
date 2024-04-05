7510dd19894b4772038f70bde7133ad6a63a130a
Huy Nguyen <hello@huynguyen.me>
2024-04-04 23:32:37 -0500

    Adding additional keybinds for filtering jumps and marks in Telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,30 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    lazy = false,
-    tag = "0.1.6",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-    keys = {
-        vim.keymap.set("n", "<leader>tf", ":Telescope find_files<CR>"),
-        vim.keymap.set("n", "<leader>tl", ":Telescope live_grep<CR>"),
-        vim.keymap.set("n", "<leader>tg", ":Telescope live_grep<CR>"),
-        vim.keymap.set("n", "<leader>tb", ":Telescope buffers<CR>"),
-        vim.keymap.set("n", "<leader>th", ":Telescope help_tags<CR>"),
-    },
-    config = function()
-        local telescope = require("telescope")
+	"nvim-telescope/telescope.nvim",
+	lazy = false,
+	tag = "0.1.6",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	keys = {
+		vim.keymap.set("n", "<leader>tf", ":Telescope find_files<CR>"),
+		vim.keymap.set("n", "<leader>tl", ":Telescope live_grep<CR>"),
+		vim.keymap.set("n", "<leader>tg", ":Telescope live_grep<CR>"),
+		vim.keymap.set("n", "<leader>tb", ":Telescope buffers<CR>"),
+		vim.keymap.set("n", "<leader>th", ":Telescope help_tags<CR>"),
+		vim.keymap.set("n", "<leader>tm", ":Telescope marks<CR>"),
+		vim.keymap.set("n", "<leader>tj", ":Telescope jumplist<CR>"),
+	},
+	config = function()
+		local telescope = require("telescope")
 
-        telescope.setup({
-            defaults = {
-                layout_strategy = "vertical",
-                layout_config = { prompt_position = "bottom" },
-            },
-        })
+		telescope.setup({
+			defaults = {
+				layout_strategy = "vertical",
+				layout_config = { prompt_position = "bottom" },
+			},
+		})
 
-        telescope.load_extension("fzf")
-    end,
+		telescope.load_extension("fzf")
+	end,
 }
