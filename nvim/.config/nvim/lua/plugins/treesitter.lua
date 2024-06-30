99acda49a4840b73b99d73763b9ba74b80066b4c
Huy Nguyen <hello@huynguyen.me>
2024-06-30 13:51:35 -0500

    feat: adding godot support

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,46 @@
 local config = function()
-    require("nvim-treesitter.configs").setup({
-        indent = {
-            enable = true,
-        },
-        autotag = {
-            enable = true,
-        },
-        ensure_installed = {
-            "markdown",
-            "json",
-            "javascript",
-            "typescript",
-            "yaml",
-            "html",
-            "css",
-            "markdown",
-            "bash",
-            "lua",
-            "dockerfile",
-            "gitignore",
-            "python",
-            "go",
-            "java",
-            "make",
-            "scss",
-            "terraform",
-            "tmux",
-            "toml",
-        },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = true,
-        },
-    })
+	require("nvim-treesitter.configs").setup({
+		indent = {
+			enable = true,
+		},
+		autotag = {
+			enable = true,
+		},
+		ensure_installed = {
+			"markdown",
+			"json",
+			"javascript",
+			"typescript",
+			"yaml",
+			"html",
+			"css",
+			"markdown",
+			"bash",
+			"lua",
+			"dockerfile",
+			"gitignore",
+			"python",
+			"go",
+			"java",
+			"make",
+			"scss",
+			"terraform",
+			"tmux",
+			"toml",
+			"gdscript",
+			"godot_resource",
+			"gdshader",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = true,
+		},
+	})
 end
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    config = config
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	config = config,
 }
-
