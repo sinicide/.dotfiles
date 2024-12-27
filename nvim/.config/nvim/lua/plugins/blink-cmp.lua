4a2f524fe69ab9dc5b8311e1f068ec2209abfa9d
Huy Nguyen <hello@huynguyen.me>
2024-12-27 13:45:18 -0600

    fix: Fixing blink.cmp autocompletion issue in telescope. Github Issue 802

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -19,7 +19,16 @@ return {
 				keyword = { range = "full" },
 				list = { selection = "auto_insert" },
 				documentation = { auto_show = true, auto_show_delay_ms = 500, treesitter_highlighting = true },
-				menu = { enabled = true, scrolloff = 2, scrollbar = true, auto_show = true },
+				menu = {
+					enabled = true,
+					scrolloff = 2,
+					scrollbar = true,
+					auto_show = function()
+						return vim.bo.buftype ~= "prompt"
+							and vim.b.completion ~= false
+							and vim.bo.filetype ~= "TelescopePrompt"
+					end,
+				},
 				ghost_text = { enabled = true },
 			},
 			signature = { enabled = true },
