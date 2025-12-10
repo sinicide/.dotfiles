3ca3c4b2d10523e7d60e9aad67867045a49b72bf
Huy Nguyen <hello@huynguyen.me>
2025-12-09 18:02:54 -0600

    feat: adding error handling

--- nvim/.config/nvim/lua/packagemanager/init.lua
+++ nvim/.config/nvim/lua/packagemanager/init.lua
@@ -1,22 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup('plugins', {
-    checker = {
-        enabled = true,
-        notify = false,
-    },
-    change_detection = {
-        nottify = false,
-    },
+require("lazy").setup("plugins", {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		nottify = false,
+	},
 })
